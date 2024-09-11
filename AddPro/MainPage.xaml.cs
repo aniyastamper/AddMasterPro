2ab70f14c4969881e3c55a5eb002f040c5cfafc3
Aniya Stamper <300091463@fvtc.edu>
2024-09-11 16:53:07 -0700

    Add Master Pro Works

---
+++ AddPro/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace AddPro;
+
+public partial class MainPage : ContentPage
+{
+    
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Add Master Pro";
+    }
+
+
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        txtFirstNumber.Text =String.Empty;
+        txtSecondNumber.Text =String.Empty;
+        lblDisplay.Text = "0";
+    }
+
+    private void Add_OnClicked(object sender, EventArgs e)
+    {
+        Double dblFirst,dblSecond;
+        Double.TryParse(txtFirstNumber.Text, out dblFirst);
+        Double.TryParse(txtSecondNumber.Text, out dblSecond);
+
+        lblDisplay.Text = (dblFirst + dblSecond).ToString();
+    }
+}
